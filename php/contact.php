fdbcf8b38a71464e94be718ce54a607421246c15
PietVlem <pieter.vleminckx@student.arteveldehs.be>
2017-10-13 15:44:02 +0200

    update

--- php/contact.php
+++ php/contact.php
@@ -1,28 +1,36 @@
 <?php
-	//variables
-	$email_to = "pieter.vlem@gmail.com";
-    $email_subject = "Contact form esign";
- 
-    $name = $_POST['name_input']; 
-    $mail = $_POST['mail_input']; 
-    $company = $_POST['company_input']; 
-    $phone = $_POST['phone_input']; 
-	$message = $_POST['message_input'];  
- 
-    $email_message .= "name: ".$name."\n";
-    $email_message .= "E-mail: ".$mail."\n";
-    $email_message .= "company: ".$company."\n";
-    $email_message .= "Telephone: ".$phone."\n\n";
-    $email_message .= "Comments: "."\n".$message."\n";
- 
-	// create email headers
-	$headers = 'From: '.$mail."\r\n".
-	'Reply-To: '.$mail."\r\n" .
-	'X-Mailer: PHP/' . phpversion();
+    if(!empty($_POST['name_input']) && !empty($_POST['mail_input']) && !empty($_POST['phone_input']) && !empty($_POST['message_input']))
+    {
+        //variables
+        $email_to = "pieter.vlem@gmail.com";
+        $email_subject = "Contact form esign";
 
-	//mail
-	@mail($email_to, $email_subject, $email_message, $headers); 
+        $name = $_POST['name_input']; 
+        $mail = $_POST['mail_input']; 
+        $company = $_POST['company_input']; 
+        $phone = $_POST['phone_input']; 
+        $message = $_POST['message_input'];  
 
-	//return
-	header('Location: ../index.html');
+        $email_message .= "name: ".$name."\n";
+        $email_message .= "E-mail: ".$mail."\n";
+        $email_message .= "company: ".$company."\n";
+        $email_message .= "Telephone: ".$phone."\n\n";
+        $email_message .= "Comments: "."\n".$message."\n";
+
+        // create email headers
+        $headers = 'From: '.$mail."\r\n".
+        'Reply-To: '.$mail."\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+        //mail
+        @mail($email_to, $email_subject, $email_message, $headers); 
+
+        //return
+        header('Location: ../index.html');
+    }
+    else
+    {
+        //return
+        header('Location: ../index.html');
+    }
 ?>
