155f01a8eca0e5a5a627d95c81c10ce4792119cc
PietVlem <pieter.vleminckx@student.arteveldehs.be>
2017-10-13 13:37:58 +0200

    init...

---
+++ js/app.js
@@ -0,0 +1,61 @@
+new Vue({
+    el: '#root',
+    data:{
+        name: "",
+        errorname: false,
+        mail: "",
+        errormail: false,
+        phone: "",
+        errorphone: false,
+        btnDisable: false,
+    },
+    methods:{
+        disableEnableBtn(){
+            (this.errormail && this.errorname) ? this.active = false : this.active = true;
+            console.log("test")
+        },
+        validateName(){
+            if(this.name=="")
+            { 
+                this.errorname = false;
+                return;
+            }
+            const reName = /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/;
+            const nameTest = reName.test(this.name);
+            (!nameTest) ? this.errorname = true: this.errorname = false;
+            (this.errormail || this.errorname) ? this.btnDisable = true : this.btnDisable = false
+        },
+        resetErrorName(){
+            this.errorname = false;
+        },
+        validateEmail(){
+            if(this.mail=="")
+            { 
+                this.errormail = false;
+                return;
+            }
+            const reMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            const emailTest = reMail.test(this.mail);
+            (!emailTest) ? this.errormail = true: this.errormail = false;
+            (this.errormail || this.errorname) ? this.btnDisable = true : this.btnDisable = false
+        },
+        resetErrorMail(){
+            this.errormail = false;
+        },
+        validatePhone(){
+            if(this.phone=="")
+            { 
+                this.phone = false;
+                return;
+            }
+            const rePhone = new RegExp('^[0-9]+$');
+            const phoneTest = rePhone.test(this.phone);
+            console.log(phoneTest);
+            (!phoneTest) ? this.errorphone = true: this.errorphone = false;
+        },
+        resetErrorPhone(){
+            this.errorphone = false;
+        },
+    },
+});
+
---
+++ php/contact.php
@@ -0,0 +1,28 @@
+<?php
+	//variables
+	$email_to = "pieter.vlem@gmail.com";
+    $email_subject = "Contact form esign";
+ 
+    $name = $_POST['name_input']; 
+    $mail = $_POST['mail_input']; 
+    $company = $_POST['company_input']; 
+    $phone = $_POST['phone_input']; 
+	$message = $_POST['message_input'];  
+ 
+    $email_message .= "name: ".$name."\n";
+    $email_message .= "E-mail: ".$mail."\n";
+    $email_message .= "company: ".$company."\n";
+    $email_message .= "Telephone: ".$phone."\n\n";
+    $email_message .= "Comments: "."\n".$message."\n";
+ 
+	// create email headers
+	$headers = 'From: '.$mail."\r\n".
+	'Reply-To: '.$mail."\r\n" .
+	'X-Mailer: PHP/' . phpversion();
+
+	//mail
+	@mail($email_to, $email_subject, $email_message, $headers); 
+
+	//return
+	header('Location: ../index.html');
+?>
